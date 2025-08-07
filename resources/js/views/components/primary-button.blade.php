a5829ed63e6c90ea6a330349b0e911ca3994b300
Indah Febrianti Makini <133872197+Makeeny@users.noreply.github.com>
2025-08-08 00:18:42 +0700

    Delete resources/js directory

--- resources/js/views/components/primary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-3 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
--- resources/js/views/siswa/notifications.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.siswa')
-@section('title', 'Notifikasi')
-@section('content')
-    <h1>Halaman Notifikasi</h1>
-    <p>Semua notifikasi akan ditampilkan di sini.</p>
-@endsection
